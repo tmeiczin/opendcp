552c2443ff973c97807d738b5412a1d31c0398d9
Terrence Meiczinger <terrence72#gmail.com>
2013-07-28 20:37:14 -0400

    added flag to change mode to raw

--- libopendcp/opendcp.h.cmake
+++ libopendcp/opendcp.h.cmake
@@ -357,7 +357,7 @@ void  opendcp_log_init(int level);
 void  dcp_fatal(opendcp_t *opendcp, char *error, ...);
 void  get_timestamp(char *timestamp);
 int   get_asset_type(asset_t asset);
-int   get_file_essence_class(char *filename);
+int   get_file_essence_class(char *filename, int raw);
 int   validate_reel(opendcp_t *opendcp, reel_t *reel, int reel_number);
 int   add_asset(opendcp_t *opendcp, asset_t *asset, char *filename);
 int   add_asset_to_reel(opendcp_t *opendcp, reel_t *reel, asset_t asset);
