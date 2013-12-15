9aea40f8a85420e2f15d37fff1411fc42fd4342d
Terrence Meiczinger <terrence72#gmail.com>
2013-12-14 21:52:31 -0500

    add strndup for win32

---
+++ cli/win32/opendcp_win32_string.h
@@ -0,0 +1,19 @@
+#include <stddef.h>
+
+char *strndup (const char *s, size_t n) {
+    char *c;
+    size_t len = strlen(s);
+
+    if (n < len) {
+        len = n;
+    }
+
+    c = (char *) malloc (len + 1);
+    if (c == NULL) {
+        return 0;
+    }
+
+    c[len] = '\0';
+
+    return (char *)memcpy (c, s, len);
+}
