a84c5d0359c39392c855bc31d46fb198ae0e1d35
Terrence Meiczinger <terrence72#gmail.com>
2014-01-20 10:13:55 -0500

    updated

--- scripts/make_lut.py
+++ scripts/make_lut.py
@@ -28,96 +28,99 @@ def defines():
     t += "#define BIT_DEPTH      %s\n" % (BIT_DEPTH)
     t += "#define BIT_PRECISION  %s\n" % (BIT_PRECISION)
     t += "#define COLOR_DEPTH    %s\n" % (COLOR_DEPTH) 
+    t += "#define HEADROOM       %s\n" % (HEADROOM) 
     t += "#define DCI_LUT_SIZE   %s\n" % ("(COLOR_DEPTH + 1) * BIT_PRECISION")
     t += "#define DCI_GAMMA      %s\n" % (DCI_GAMMA)
     t += "#define DCI_DEGAMMA    %s\n" % ("1/DCI_GAMMA")
-    t += "#define DCI_COEFFICENT %s"   % (DCI_COEFFICENT)
+    t += "#define DCI_COEFFICENT %s"   % (DCI_COEFFICENT_S)
 
     return t
 
-enums = '''\
-enum COLOR_PROFILE_ENUM {
-    CP_SRGB = 0,
-    CP_REC709,
-    CP_DC28,
-    CP_MAX
-};
+def enums():
+    f = " = 0"
+    t = "enum COLOR_PROFILE_ENUM {\n"
+    for c in color_spaces:
+        t += "    CP_%s%s,\n" % (c['id'].upper(), f)
+        f = ''
+    t += "    CP_MAX\n"
+    t += "};\n\n"
 
-enum LUT_IN_ENUM {
-    LI_SRGB    = 0,
-    LI_REC709,
-    LI_MAX
-};
+    f = " = 0"
+    t += "enum LUT_IN_ENUM {\n"
+    for c in color_spaces:
+        t += "    LI_%s%s,\n" % (c['id'].upper(), f)
+        f = ''
+    t += "    LI_MAX\n"
+    t += "};\n\n"
 
-enum LUT_OUT_ENUM {
-    LO_DCI    = 0,
-    LO_MAX
-};\
-'''
+    f = " = 0"
+    t += "enum LUT_OUT_ENUM {\n"
+    for c in color_spaces_out:
+        t += "    LO_%s%s,\n" % (c['id'].upper(), f)
+        f = ''
+    t += "    LO_MAX\n"
+    t += "};"
 
-color_matrix = '''\
-static float GAMMA[3] = {
-    2.4,    /* SRGB    */
-    2.2,    /* REC709  */
-    2.6     /* DCI     */
-};
+    return t
 
-/* Color Matrices */
-static float color_matrix[3][3][3] = {
-    /* SRGB */
-    {{0.4124564, 0.3575761, 0.1804375},
-     {0.2126729, 0.7151522, 0.0721750},
-     {0.0193339, 0.1191920, 0.9503041}},
+def gamma():
+    l = len(color_spaces)
 
-    /* REC.709 */
-    {{0.4124564, 0.3575761, 0.1804375},
-     {0.2126729, 0.7151522, 0.0721750},
-     {0.0193339, 0.1191920, 0.9503041}},
+    t = "static float GAMMA[%d] = {\n" % (l)
+    for i in range(0, l):
+        c = color_spaces[i]
+        t += "     %f,    /* %-8.8s */\n" % (c['gamma'], c['id'].upper())
+    t += "};"
 
-    /* DC28.30 (2006-02-24) */
-    {{0.4451698156, 0.2771344092, 0.1722826698},
-     {0.2094916779, 0.7215952542, 0.0689130679},
-     {0.0000000000, 0.0470605601, 0.9073553944}}
-};\
-'''
+    return t
+    
+def color_matrix():
+    l = len(color_spaces)
 
-footer = "#endif //_OPEN_DCP_XYZ_H_"
+    t = "static float color_matrix[%d][3][3] = {\n" % (l)
+    for i in range(0, l):
+        t += "    /* %s */\n" % (color_spaces[i]['id'].upper())
+        t += "    {{%s},\n" % (", ".join(str(i) for i in color_spaces[i]['matrix'][0]))
+        t += "     {%s},\n" % (", ".join(str(i) for i in color_spaces[i]['matrix'][1]))
+        t += "     {%s}},\n" % (", ".join(str(i) for i in color_spaces[i]['matrix'][2]))
+        if i < l - 1:
+            t += "\n"
+    t += "};"
 
-def calculate_srgb(i):
-    p = float(i) / (BIT_LENGTH - 1.0)
+    return t
+    
+def footer():
+    t = "#endif //_OPEN_DCP_XYZ_H_"
 
-    if p > 0.04045:
-        v = ((p+0.055)/1.055)**SRGB_GAMMA
-    else:
-        v = p/12.92
+    return t
 
-    return v
+def test(source):
+    for c in color_spaces:
+       if c['id'] == source:
+           print "found %s" % (c['id'])
+           f = 'calculate_'+c['id']
+           m = c['matrix']
 
-def calculate_rec709(i):
-    p = float(i) / (BIT_LENGTH - 1.0)
+    if not m: return
 
-    if p > 0.08125:
-        v = ((p+0.099)/1.099)**REC709_GAMMA
-    else: 
-        v = p/4.50
+    for i in range(0, BIT_LENGTH):
+        r = g = b = eval(f)(i)
+        p = float(i) / (BIT_LENGTH - 1.0)
+        if p > 0.08125:
+           n = "*"
+        else:
+           n = ''
 
-    return v
+        xn = r*m[0][0] + g*m[0][1] + b*m[0][2]
+        yn = r*m[1][0] + g*m[1][1] + b*m[1][2]
+        zn = r*m[2][0] + g*m[2][1] + b*m[2][2]
 
-def test():
-    rec709 = color_spaces['rec709']['matrix']
-    print "bit length: %s" % (BIT_LENGTH)
-    for i in range(160, 165):
-        r = g = b = calculate_rec709(i)
+        x = int(((xn*DCI_COEFFICENT) ** (1/DCI_GAMMA)) * (BIT_LENGTH - 1.0))
+        y = int(((yn*DCI_COEFFICENT) ** (1/DCI_GAMMA)) * (BIT_LENGTH - 1.0))
+        z = int(((zn*DCI_COEFFICENT) ** (1/DCI_GAMMA)) * (BIT_LENGTH - 1.0)) 
 
-        x = r*rec709[0][0] + g*rec709[0][1] + b*rec709[0][2]
-        y = r*rec709[1][0] + g*rec709[1][1] + b*rec709[1][2]
-        z = r*rec709[2][0] + g*rec709[2][1] + b*rec709[2][2]
-
-        x = int(((x*DCI_COEFFICENT) ** (1/DCI_GAMMA)) * (BIT_LENGTH - 1.0) - 256)
-        y = int(((y*DCI_COEFFICENT) ** (1/DCI_GAMMA)) * (BIT_LENGTH - 1.0) - 256)
-        z = int(((z*DCI_COEFFICENT) ** (1/DCI_GAMMA)) * (BIT_LENGTH - 1.0) - 256) 
-
-        print "(%d, %d, %d) -> (%d, %d, %d)" % (i,i,i,x,y,x)
+        print "%d, %f" % (i, yn)
+        #print "%d, %d, %d | %f, %f, %f | %f, %f, %f | %f, %f, %f | %d, %d, %d | %s" % (i,i,i,p,p,p,r,g,b,xn,yn,zn,x,y,z,n)
 
 def generate_lut_in(lut):
     t = ''
@@ -145,7 +148,7 @@ def generate_lut_in(lut):
 def generate_lut_out(lut):
     c = 0
     t = ''
-    for x in range(0, BIT_LENGTH*BIT_PRECISION):
+    for x in range(0, BIT_LENGTH * BIT_PRECISION):
         v = int(((x/(BIT_LENGTH * BIT_PRECISION - 1.0)) ** (1/DCI_GAMMA)) * (BIT_LENGTH - 1.0))
 
         if c == 0:
@@ -174,8 +177,14 @@ def calculate_srgb(i):
 
     return v
 
+def calculate_dci(i):
+    p = float(i) / (BIT_LENGTH - 1.0) / DCI_COEFFICENT
+    v = p ** DCI_GAMMA
+
+    return v
+
 def calculate_rec709(i):
-    p = float(i) / (BIT_LENGTH - 1.0)
+    p = float(i) / (BIT_LENGTH - 1.0) 
 
     if p > 0.08125:
         v = ((p+0.099)/1.099)**REC709_GAMMA
@@ -184,24 +193,22 @@ def calculate_rec709(i):
 
     return v
 
-color_spaces = {'rec709': {'name': "REC. 709", 'gamma': 1.0/0.45, 'matrix': [[0.4124564, 0.3575761, 0.1804375],[0.4124564, 0.3575761, 0.1804375], [0.0193339, 0.1191920, 0.9503041]]},
-                'srgb':   {'name': "sRGB", 'gamma': 2.4, 'matrix': [[]]}
-               }
-
-def print_lut_in():
+def lut_in():
     t = "static float lut_in[LI_MAX][COLOR_DEPTH+1] = {\n"
-    for key in color_spaces:
+    for i in range(0, len(color_spaces)):
         t += "    // Bit Depth:       %s\n" % (BIT_DEPTH)
-        t += "    // Reference White: %s\n" % (color_spaces[key]['name'])
-        t += "    // Gamma:           %s\n" % (color_spaces[key]['gamma'])
+        t += "    // Reference White: %s\n" % (color_spaces[i]['id'].upper())
+        t += "    // Gamma:           %s\n" % (color_spaces[i]['gamma'])
         t += "    {\n";
-        t += generate_lut_in(key)
-        t += "    },\n\n"
-    t += "};\n"
+        t += generate_lut_in(color_spaces[i]['id'])
+        t += "    },\n"
+        if i < len(color_spaces) - 1:
+            t += "\n"
+    t += "};"
 
     return t
 
-def print_lut_out():
+def lut_out():
     t  = "static int lut_out[1][DCI_LUT_SIZE] = {\n"
     t += "    // Bit Depth:       %s\n" % (BIT_DEPTH)
     t += "    // Reference White: %s\n" % ("DCI")
@@ -209,26 +216,56 @@ def print_lut_out():
     t += "    {\n"
     t += generate_lut_out("dci")
     t += "    }\n"
-    t += "};\n"
+    t += "};"
 
     return t
 
-BIT_DEPTH      = 12
-COLOR_DEPTH    = 2**BIT_DEPTH - 1
-BIT_PRECISION  = 16
-BIT_LENGTH     = 2**BIT_DEPTH
-DCI_GAMMA      = 2.6
-SRGB_GAMMA     = 2.4
-REC709_GAMMA   = 1/0.45
-DCI_COEFFICENT = 48.0/52.37
+color_spaces = [
+                { 
+                 'id':     'srgb',
+                 'gamma':  2.4,
+                 'matrix': [[0.4124564, 0.3575761, 0.1804375],
+                            [0.2126729, 0.7151522, 0.0721750],
+                            [0.0193339, 0.1191920, 0.9503041]]},
+                { 
+                 'id':     'rec709',
+                 'gamma':  1.0/0.45,
+                 'matrix': [[0.4123907993, 0.3575843394, 0.1804807884],
+                            [0.2126390059, 0.7151686778, 0.0721923154],
+                            [0.0193308187, 0.1191947798, 0.9505321522]]},
+               ]
 
-test()
-sys.exit(1)
+color_spaces_out = [
+                    {
+                    'id':    'dci',
+                    'gamma': 1/2.6,
+                    'matrix': [[0.4123907993, 0.3575843394, 0.1804807884],
+                               [0.2126390059, 0.7151686778, 0.0721923154],
+                               [0.0193308187, 0.1191947798, 0.9505321522]]},
+                   ]
+
+BIT_DEPTH       = 12 
+HEADROOM        = int(16 * 2**(BIT_DEPTH-8))
+COLOR_DEPTH     = 2**BIT_DEPTH - 1
+BIT_PRECISION   = 16
+BIT_LENGTH      = 2**BIT_DEPTH
+DCI_GAMMA       = 2.6
+SRGB_GAMMA      = 2.4
+REC709_GAMMA    = 1/.45 
+DCI_COEFFICENT  = 48.0/52.37
+DCI_COEFFICENT_S = "48.0/52.37"
+
+if len(sys.argv) > 1: 
+    if sys.argv[1] == "test":
+        test("rec709")
+        #test("srgb")
+    sys.exit(1)
 
 print "%s\n" % (header)
 print "%s\n" % (defines())
-print "%s\n" % (enums)
-print "%s\n" % (color_matrix)
-print "%s\n" % (print_lut_in())
-print "%s\n" % (print_lut_out())
-print "%s" % (footer)
+print "%s\n" % (enums())
+print "%s\n" % (gamma())
+print "%s\n" % (color_matrix())
+print "%s\n" % (lut_in())
+print "%s\n" % (lut_out())
+print "%s"   % (footer())
