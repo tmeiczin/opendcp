8d4141637b5801d6fde59312a8c54a24d18709ed
SieuDoTia <su_huynh@yahoo.com>
2016-09-28 09:49:38 +0700

    Update opendcp_decoder_exr.c

--- libopendcp/codecs/opendcp_decoder_exr.c
+++ libopendcp/codecs/opendcp_decoder_exr.c
@@ -8,11 +8,11 @@
 #include "opendcp.h"
 #include "opendcp_image.h"
 
-#define MAGIC_NUMBER 0x762f3101
+#define MAGIC_NUMBER_EXR 0x762f3101
 
 typedef enum {
-    BMP_TOP         = 0,
-    BMP_BOTTOM      = 1,
+    EXR_TOP         = 0,
+    EXR_BOTTOM      = 1,
 } line_order;
 
 typedef enum {
@@ -29,11 +29,11 @@ typedef enum {
 } exr_compression_enum;
 
 typedef struct {
-    uint32_t magic_num;         /* magic number 0x762f3101        */
+    uint32_t magic_num;         /* magic number 0x762f3101 (file signature) */
 } exr_magic_num_t;
 
 typedef struct {
-    uint16 num_channel;         /* magic number 0x762f3101        */
+    uint16 num_channel;
 } exr_channel_list;
 
 typedef struct {
@@ -46,6 +46,7 @@ typedef struct {
     int                      row_order;
 } exr_image_t;
 
+#pragma mark ---- Half --> Float
 /* for change half become float */
 typedef union {
     unsigned int i;
@@ -86,25 +87,25 @@ unsigned char shiftForNumber( unsigned short value ) {
 
 float half2float( unsigned short half ) {
     
-    u_intfloat int_fl;
+   u_intfloat int_fl;
 
-    // normal number +
-    if( (half > 0x03ff) && (half < 0x7c00) ) {
-       unsigned int exponent = (((half & 0x7c00) >> 10) + 112) << 23;
-       unsigned int value = (half & 0x03ff) << 13;
-       int_fl.i = exponent | value;
-    }
-     // normal number -
-    else if( (half > 0x83ff) && (half < 0xfc00) ) {
-       unsigned int exponent = (((half & 0x7c00) >> 10) + 112) << 23;
-       unsigned int value = (half & 0x03ff) << 13;
-       int_fl.i = 0x80000000 + exponent + value;
-    }
-    // not normal number +
-    else if( (half > 0x0000) && (half < 0x0400) ) {
-       unsigned char shift = shiftForNumber( half );
-       unsigned int exponent = (shift + 102) << 23;
-       unsigned int value = (half << (24 - shift)) & 0x007fffff;
+   // normal number +
+   if( (half > 0x03ff) && (half < 0x7c00) ) {
+      unsigned int exponent = (((half & 0x7c00) >> 10) + 112) << 23;
+      unsigned int value = (half & 0x03ff) << 13;
+      int_fl.i = exponent | value;
+   }
+   // normal number -
+   else if( (half > 0x83ff) && (half < 0xfc00) ) {
+      unsigned int exponent = (((half & 0x7c00) >> 10) + 112) << 23;
+      unsigned int value = (half & 0x03ff) << 13;
+      int_fl.i = 0x80000000 + exponent + value;
+   }
+   // not normal number +
+   else if( (half > 0x0000) && (half < 0x0400) ) {
+      unsigned char shift = shiftForNumber( half );
+      unsigned int exponent = (shift + 102) << 23;
+      unsigned int value = (half << (24 - shift)) & 0x007fffff;
       int_fl.i = exponent + value;
     }
     // not normal number -
@@ -113,30 +114,83 @@ float half2float( unsigned short half ) {
        unsigned int exponent = (shift + 102) << 23;
        unsigned int value = (half << (24 - shift)) & 0x007fffff;
       int_fl.i = 0x80000000 + exponent + value;
-    }
-    // zero +
-    else if( half == 0x0000 )
-        int_fl.i = 0x00000000;
+   }
+   // zero +
+   else if( half == 0x0000 )
+      int_fl.i = 0x00000000;
  
-    // zero -
-    else if( half == 0x8000 )
-        int_fl.i = 0x80000000;
+   // zero -
+   else if( half == 0x8000 )
+      int_fl.i = 0x80000000;
  
-    // infinity +
-    else if( half == 0x7c00 ) 
-        int_fl.i = 0x7f800000;
+   // infinity +
+   else if( half == 0x7c00 ) 
+      int_fl.i = 0x7f800000;
   
-    // infinity -
-    else if( half == 0xfc00 )
-        int_fl.i = 0xff800000;
+   // infinity -
+   else if( half == 0xfc00 )
+      int_fl.i = 0xff800000;
 
-    // NaN +
-    else if( (half > 0x7c00) && (half < 0x8000) ) 
-        int_fl.i = ((half & 0x3ff) << 13) | 0x7f800000;
+   // NaN +
+   else if( (half > 0x7c00) && (half < 0x8000) ) 
+      int_fl.i = ((half & 0x3ff) << 13) | 0x7f800000;
  
-    // NaN -
-    else if( half > 0xfc00 )
-        int_fl.i = ((half & 0x3ff) << 13) | 0xff800000;
+   // NaN -
+   else if( half > 0xfc00 )
+      int_fl.i = ((half & 0x3ff) << 13) | 0xff800000;
 
-    return int_fl.f;
+   return int_fl.f;
+}
+
+#pragma mark ---- Read EXR File
+int opendcp_decode_exr(opendcp_image_t **image_ptr, const char *sfile) {
+
+   FILE *exr_fp;
+    
+      /* open exr using filename or file descriptor */
+   OPENDCP_LOG(LOG_DEBUG,"%-15.15s: opening exr file %s","read_exr",sfile);
+
+   exr_fp = fopen(sfile, "rb");
+    
+   if (!exr_fp) {
+      OPENDCP_LOG(LOG_ERROR,"%-15.15s: opening exr file %s","read_bmp",sfile);
+      return OPENDCP_FATAL;
+   }
+    
+   // ---- check magic number/file signature
+   unsigned int magicNumber = 0x0;
+   // ---- careful about endian 
+   magicNumber = fgetc(exr_fp) << 24;
+   magicNumber |= fgetc(exr_fp) << 16;
+   magicNumber |= fgetc(exr_fp) << 8;
+   magicNumber |= fgetc(exr_fp);
+    
+   if (readsize != MAGIC_NUMBER_EXR ) {
+      OPENDCP_LOG(LOG_ERROR,"%-15.15s: failed to read magic number expected 0x%08x read 0x%08x","read_exr", MAGIC_NUMBER_EXR, magicNumber );
+      OPENDCP_LOG(LOG_ERROR,"%s is not a valid EXR file", sfile);
+      return OPENDCP_FATAL;
+   }
+   
+   // ---- version
+   unsigned char version = fgetc(exr_fp);
+   if( version != 2 ) {
+     OPENDCP_LOG(LOG_ERROR,"Only support exr file version 2, this file version %d", version);
+     return OPENDCP_FATAL;
+   }
+
+   // ---- file type: normal, deep pixel, multipart (only support normal)
+   unsigned char type = fgetc(exr_fp);
+   if( type & 0x1b != 0x00 ) {
+      OPENDCP_LOG(LOG_ERROR,"Only support normal scanline exr file, no tile, deep pixel, multipart file");
+     return OPENDCP_FATAL;
+   }
+   
+   // ---- skip two bytes, not used
+   fgetc(exr_fp);
+   fgetc(exr_fp);
+  
+   // ---- read standard EXR attritubes
+   
+       
+   // ---- 
 }
